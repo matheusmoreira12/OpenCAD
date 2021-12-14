f41579c327c9a34d900d20060f7b6bc4c482db63
MMM <matheusmatos32@gmail.com>
2021-12-14 00:19:29 -0300

    Renamed references / Fixes in Tokens module

--- Libraries/Modules/Tokens/Tokens/StringTokenifier.cs
+++ Libraries/Modules/Tokens/Tokens/StringTokenifier.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace OpenCAD.APIs.Tokens
+namespace OpenCAD.Modules.Tokens
 {
 
     public abstract class StringTokenifier<TToken> where TToken: StringToken
