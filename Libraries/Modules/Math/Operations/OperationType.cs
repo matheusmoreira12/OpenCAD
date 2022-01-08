372e7d4f069fd2cf5dfb8b32b6a67f9c1391bd09
MMM <matheusmatos32@gmail.com>
2022-01-08 16:37:03 -0300

    Update OperationType.cs
    Refactoring

--- Libraries/Modules/Math/Operations/OperationType.cs
+++ Libraries/Modules/Math/Operations/OperationType.cs
@@ -4,19 +4,27 @@ namespace OpenCAD.Modules.Math.Operations
 {
     public partial class OperationType : ExpandableEnum<OperationType>
     {
+        #region Basic Unary Operations
         public static readonly OperationType Positivation = new OperationType(0);
         public static readonly OperationType Negation = new OperationType();
+        #endregion
+
+        #region Basic Binary Operations
         public static readonly OperationType Addition = new OperationType();
         public static readonly OperationType Subtraction = new OperationType();
         public static readonly OperationType Multiplication = new OperationType();
         public static readonly OperationType Division = new OperationType();
         public static readonly OperationType Exponentiation = new OperationType();
+        #endregion
+
+        #region Trigonometric Operations
         public static readonly OperationType Sine = new OperationType();
         public static readonly OperationType Cosine = new OperationType();
         public static readonly OperationType Tangent = new OperationType();
         public static readonly OperationType Cosecant = new OperationType();
         public static readonly OperationType Secant = new OperationType();
         public static readonly OperationType Cotangent = new OperationType();
+        #endregion
 
         public OperationType() { }
 
