12d75a49a6d6f9f450cff1c5d6b5acb0a452255e
MMM <matheusmatos32@gmail.com>
2022-01-08 14:44:10 -0300

    Update MathOperationManager.cs
    - Fixes operation matching
    - Betters documentation
    - Refactors and corrects minor nomenclature mistakes

--- Libraries/Modules/Math/Operations/MathOperationManager.cs
+++ Libraries/Modules/Math/Operations/MathOperationManager.cs
@@ -7,13 +7,22 @@ namespace OpenCAD.Modules.Math.Operations
 {
     public static class MathOperationManager
     {
-        private static List<NAryOperation> registeredOperations { get; } = new List<NAryOperation> { };
+        private static WarningException GetAlreadyRegisteredWarning(NAryOperation operation)
+        {
+            var operationType = operation.OperationType;
+            var operandTypes = operation.OperandTypes;
+            var operandTypeNames = operandTypes.Select(type => type.Name);
+            string operandTypesStr = $"{string.Join(", ", operandTypeNames)}";
+            string message = $"Refused to register Math operation. The {operation.OperationType} operation has already been registered with operand type(s) {operandTypesStr}.";
+            return new WarningException(message);
+        }
+
         public static void Register(NAryOperation operation)
         {
             if (IsRegistered(operation.OperationType, operation.OperandTypes))
-                throw new WarningException("Refused to register Math operation. The specified Math operation type has already been registered with the specified operand types.");
+                throw GetAlreadyRegisteredWarning(operation);
 
-            registeredOperations.Add(operation);
+            RegisteredOperations.Add(operation);
         }
 
         public static void RegisterMany(IList<NAryOperation> operations)
@@ -22,7 +31,7 @@ namespace OpenCAD.Modules.Math.Operations
                 Register(operation);
         }
 
-        public static void Unregister(NAryOperation operation) => registeredOperations.Remove(operation);
+        public static void Unregister(NAryOperation operation) => RegisteredOperations.Remove(operation);
 
         public static void UnregisterMany(IList<NAryOperation> operations)
         {
@@ -30,19 +39,51 @@ namespace OpenCAD.Modules.Math.Operations
                 Unregister(operation);
         }
 
-        public static IReadOnlyCollection<NAryOperation> GetAll() => registeredOperations;
+        private static List<NAryOperation> RegisteredOperations { get; } = new List<NAryOperation> { };
 
+        /// <summary>
+        /// Gets all the registered operations.
+        /// </summary>
+        /// <returns>All the registered operations</returns>
+        public static NAryOperation[] GetAll() => RegisteredOperations.ToArray();
+
+        /// <summary>
+        /// Gets all registered operations compatible with the specified criteria.
+        /// </summary>
+        /// <param name="operationType">The desired operation type, or null for matching any operation type.</param>
+        /// <param name="operandTypes">The desired operand types, or null for matching any operand types.</param>
+        /// <param name="resultType">The desired result type, or null for matching any result type.</param>
+        /// <returns>All registered operations compatible with the specified criteria.</returns>
         public static IEnumerable<NAryOperation> GetAll(OperationType operationType = null, Type[] operandTypes = null, Type resultType = null) =>
-            GetAll().Where(o => (operationType == null || o.OperationType == operationType)
-                || (resultType == null || o.AppliesToResultType(resultType))
-                    || (operandTypes == null || o.AppliesToOperandTypes(operandTypes)));
+            RegisteredOperations.AsParallel().Where(o => (operationType == null || o.OperationType == operationType)
+                && (resultType == null || o.AppliesToResultType(resultType))
+                    && (operandTypes == null || o.AppliesToOperandTypes(operandTypes)));
 
-        public static NAryOperation Get(OperationType operationType, Type[] operandTypes, Type resultType = null) =>
-            GetAll().FirstOrDefault(o => o.OperationType == operationType
-                || o.AppliesToOperandTypes(operandTypes)
-                    || (resultType == null || o.AppliesToResultType(resultType)));
+        //TODO: Implement an optimizer for finding suitable operations
+        public static NAryOperation Get(OperationType operationType, Type[] operandTypes, Type resultType = null)
+            => GetExact(operationType, operandTypes, resultType)
+                ?? GetAll(operationType, operandTypes, resultType).FirstOrDefault();
 
+        /// <summary>
+        /// Gets the registered operation that matches exactly the specified criteria, or null if no match is found.
+        /// </summary>
+        /// <param name="operationType">The desired operation type.</param>
+        /// <param name="operandTypes">The desired operand types.</param>
+        /// <param name="resultType">The desired result type, or null for matching any result type.</param>
+        /// <returns>The registered operation that matches exactly the specified criteria.</returns>
+        public static NAryOperation GetExact(OperationType operationType, Type[] operandTypes, Type resultType = null) =>
+            RegisteredOperations.AsParallel().FirstOrDefault(o => o.OperationType == operationType
+                && o.OperandTypes.SequenceEqual(operandTypes)
+                    && (resultType == null || o.ResultType == resultType));
+
+        /// <summary>
+        /// Checks if there is an operation registered which matches exactly the specified criteria.
+        /// </summary>
+        /// <param name="operationType">The operation type to be checked.</param>
+        /// <param name="operandTypes">The operand types to be checked.</param>
+        /// <param name="resultType">The operand types to be checked, or null for matching any result type.</param>
+        /// <returns>True if there is an operation registered which matches exactly the specified criteria, false if no match is found.</returns>
         public static bool IsRegistered(OperationType operationType, params Type[] operandTypes) =>
-            !(Get(operationType, operandTypes) == null);
+            GetExact(operationType, operandTypes) != null;
     }
 }
