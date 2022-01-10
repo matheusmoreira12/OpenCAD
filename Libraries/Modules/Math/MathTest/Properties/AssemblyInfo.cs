e2198ecbbf02687d0cc80ea68b8eb8e5e602f7c5
MMM <matheusmatos32@gmail.com>
2022-01-10 18:10:46 -0300

    Added unit tests project to Math

---
+++ Libraries/Modules/Math/MathTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MathTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MathTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d89641c4-bdbe-469a-b6b1-5e0c6d0d2f2b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Libraries/Modules/Math/MathTest/TestDefaultOperations.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenCAD.Modules.Math;
+
+namespace MathTest
+{
+    [TestClass]
+    public class TestDefaultOperations
+    {
+        [TestMethod]
+        public void TestDoubleAddition()
+        {
+            double operandA = 1;
+            double operandB = 2;
+            double expectedResult = 3;
+            var actualResult = Math.Add(operandA, operandB);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestDoubleSubtraction()
+        {
+            double operandA = 3;
+            double operandB = 2;
+            double expectedResult = 1;
+            var actualResult = Math.Subtract(operandA, operandB);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestDoubleMultiplication()
+        {
+            double operandA = 2;
+            double operandB = 4;
+            double expectedResult = 8;
+            var actualResult = Math.Multiply(operandA, operandB);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestDoubleDivision()
+        {
+            double operandA = 8;
+            double operandB = 4;
+            double expectedResult = 2;
+            var actualResult = Math.Divide(operandA, operandB);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
